52de4878732b34a1876f37fbe6dcec66f2917ba6
jkozlowski223 <julokozlowskipriv@gmail.com>
2026-03-16 13:02:19 +0100

    Koniec zadania 1

--- zad_1.py
+++ zad_1.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from time import perf_counter
 
 from Crypto.Cipher import DES3
 from Crypto.Random import get_random_bytes
@@ -17,11 +18,18 @@ def process_once():
         except ValueError:
             continue
     enc_cipher = DES3.new(key, DES3.MODE_CBC)
+    enc_start = perf_counter()
     encrypted_data = enc_cipher.encrypt(pad(data, DES3.block_size))
+    enc_time = perf_counter() - enc_start
     ENC_FILE.write_bytes(encrypted_data)
 
     dec_cipher = DES3.new(key, DES3.MODE_CBC, iv=enc_cipher.iv)
+    dec_start = perf_counter()
     DEC_FILE.write_bytes(unpad(dec_cipher.decrypt(encrypted_data), DES3.block_size))
+    dec_time = perf_counter() - dec_start
+
+    print(f"Czas szyfrowania: {enc_time:.6f} s")
+    print(f"Czas deszyfrowania: {dec_time:.6f} s")
 
 if __name__ == "__main__":
     process_once()
